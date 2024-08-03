0b2e850b931530baac4d20d84177ba8333916527
garethcheah <garethcheah@gmail.com>
2024-08-03 12:51:42 -0700

    Add time-to-live property to pickups and function to destroy itself when this time has elapsed

--- Assets/Scripts/Entities/PickUp.cs
+++ Assets/Scripts/Entities/PickUp.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public abstract class Pickup : MonoBehaviour
 {
+    [SerializeField] private float _timeToLive = 15.0f;
+
     public virtual void OnPicked()
     {
         Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        _timeToLive -= Time.deltaTime;
+
+        if (_timeToLive <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
