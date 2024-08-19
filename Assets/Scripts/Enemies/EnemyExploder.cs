4a35b07734cc6cfd64defec1bbe124f345ecd32e
garethcheah <garethcheah@gmail.com>
2024-08-19 01:50:53 -0700

    Add particle effect to Exploder enemy https://github.com/garethcheah/objects-the-game/issues/6

--- Assets/Scripts/Enemies/EnemyExploder.cs
+++ Assets/Scripts/Enemies/EnemyExploder.cs
@@ -7,6 +7,10 @@ public class EnemyExploder : Enemy
     [SerializeField] private float _explodeRange = 1.5f;
     [SerializeField] private float _explodeDamage = 50.0f;
 
+    private SpriteRenderer _srEnemy;
+    private ParticleSystem _explosionParticleSystem;
+    private bool _hasAlreadyExploded = false;
+
     public EnemyExploder(string enemyName) : base(enemyName, EnemyType.Exploder)
     {
         
@@ -14,13 +18,22 @@ public class EnemyExploder : Enemy
 
     public void Explode()
     {
-        target.GetComponent<IDamageable>().GetDamage(_explodeDamage);
-        Die();
+        if (!_hasAlreadyExploded)
+        {
+            target.GetComponent<IDamageable>().GetDamage(_explodeDamage);
+            _explosionParticleSystem.Play();
+            _hasAlreadyExploded = true;
+            Destroy(_srEnemy);
+            Invoke("Die", _explosionParticleSystem.main.duration);
+        }
     }
 
     protected override void Start()
     {
         base.Start();
+
+        _srEnemy = GetComponent<SpriteRenderer>();
+        _explosionParticleSystem = GetComponentInChildren<ParticleSystem>();
 
         // Set enemy health
         health = new Health(1, 0, 1);
